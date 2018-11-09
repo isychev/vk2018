f660ee75f61472f9276115a273daff2ca4ee4d6b
grigoant <grigoant@gmail.com>
2018-11-09 17:44:48 +0300

    fix google maps

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {YMaps,Map} from 'react-yandex-maps'
+import { MapView } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>was</Text>
+        <MapView
+          style={{ flex: 1,backgroundColor: 'black',width: 500, height:500 }}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+        <Text>was</Text>
       </View>
     );
   }
