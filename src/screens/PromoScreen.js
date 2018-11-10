b9801c135f94f9aa6bcdafd902ab247f2be2fa7e
esmirnov90 <esmirnov90@yandex.ru>
2018-11-11 02:16:01 +0300

    Add main page

--- src/screens/PromoScreen.js
+++ src/screens/PromoScreen.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
+import Button from 'apsl-react-native-button'
 
 class PromoScreen extends Component {
-  static navigationOptions = {
-    title: 'Промо',
-  };
+    static navigationOptions = {
+        title: 'Главная',
+      };
 
   render() {
+    console.log(this.props.navigation);
     return (
-      <View>
-        <Text>Промо</Text>
+      <View style={styles.container}>
+        <View style={styles.promo}>
+            <Image style={styles.logo} source={require('../logo.png')}></Image>
+        </View>
+        <View style={styles.actions}>
+        <Button style={{ backgroundColor: '#e35205', borderColor: '#e35205' }} textStyle={{ color: '#ffffff' }} onPress={() => this.props.navigation.navigate('map')}>
+            Снятие наличных
+        </Button>
+        <Button style={{ backgroundColor: '#e35205', borderColor: '#e35205' }} textStyle={{ color: '#ffffff' }} onPress={() => this.props.navigation.navigate('map')}>
+            Пополнение счета
+        </Button>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: "column",
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#ffffff'
+    },
+    promo: {
+        display: 'flex',
+        flexDirection: "column",
+        alignItems: 'center',
+        flex: 1
+    },
+    logo: {
+        width: 315, height: 301,
+        top: 70,
+    },
+    actions: {
+        width: '90%',
+        bottom: 120
+    }
+});
 
 export default PromoScreen;
